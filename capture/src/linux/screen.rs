8a770ecb933228d5fe0410a635dc71638300f095
mycrl <lepidodendraceae@gamil.com>
2024-12-31 00:12:58 +0800

    update

--- capture/src/linux/screen.rs
+++ capture/src/linux/screen.rs
@@ -40,6 +40,10 @@ pub enum ScreenCaptureError {
     NotOpenDecoder,
     #[error("failed to create sw scale context")]
     CreateSWScaleContextError,
+    #[error("failed to create HWDevice context")]
+    FailedToCreateHWDeviceContext,
+    #[error("failed to create HWFrame context")]
+    FailedToCreateHWFrameContext,
 }
 
 #[derive(Default)]
@@ -50,7 +54,7 @@ impl CaptureHandler for ScreenCapture {
     type Error = ScreenCaptureError;
     type CaptureOptions = VideoCaptureSourceDescription;
 
-    // x11 Capture does not currently support multiple screens.
+    // kms capture does not currently support multiple screens.
     fn get_sources() -> Result<Vec<Source>, Self::Error> {
         Ok(vec![Source {
             index: 0,
@@ -118,11 +122,11 @@ impl CaptureHandler for ScreenCapture {
 }
 
 struct Capture {
+    options: VideoCaptureSourceDescription,
     fmt_ctx: *mut AVFormatContext,
     codec_ctx: *mut AVCodecContext,
     packet: *mut AVPacket,
     frames: [*mut AVFrame; 3],
-    hw_device_ctx: *mut AVBufferRef,
     filter_graph: *mut AVFilterGraph,
     buffersrc_ctx: *mut AVFilterContext,
     buffersink_ctx: *mut AVFilterContext,
@@ -134,13 +138,13 @@ unsafe impl Sync for Capture {}
 impl Capture {
     fn new(options: &VideoCaptureSourceDescription) -> Result<Self, ScreenCaptureError> {
         let mut this = Self {
+            options: options.clone(),
+            filter_graph: unsafe { avfilter_graph_alloc() },
             packet: unsafe { av_packet_alloc() },
+            buffersink_ctx: null_mut(),
+            buffersrc_ctx: null_mut(),
             codec_ctx: null_mut(),
             fmt_ctx: null_mut(),
-            hw_device_ctx: null_mut(),
-            filter_graph: unsafe { avfilter_graph_alloc() },
-            buffersrc_ctx: null_mut(),
-            buffersink_ctx: null_mut(),
             frames: [
                 unsafe { av_frame_alloc() },
                 unsafe { av_frame_alloc() },
@@ -148,23 +152,27 @@ impl Capture {
             ],
         };
 
-        // if unsafe {
-        //     av_hwdevice_ctx_create(
-        //         &mut this.hw_device_ctx,
-        //         AVHWDeviceType::AV_HWDEVICE_TYPE_VAAPI,
-        //         null_mut(),
-        //         null_mut(),
-        //         0,
-        //     )
-        // } < 0
-        // {
-        //     return Err(ScreenCaptureError::NotFoundInputFormat);
-        // }
-
-        // Currently you can only capture the screen in the x11 desktop environment.
+        // Currently you can only capture the screen in the desktop environment.
         let format = unsafe { av_find_input_format(PSTR::from("kmsgrab").as_ptr()) };
         if format.is_null() {
             return Err(ScreenCaptureError::NotFoundInputFormat);
+        }
+
+        // It's just in BGRA format, which is probably all that's available in the x11
+        // desktop environment.
+        let mut format_options = null_mut();
+        for (k, v) in [
+            ("format".to_string(), "bgr0".to_string()),
+            ("framerete".to_string(), options.fps.to_string()),
+        ] {
+            unsafe {
+                av_dict_set(
+                    &mut format_options,
+                    PSTR::from(k).as_ptr(),
+                    PSTR::from(v).as_ptr(),
+                    0,
+                );
+            }
         }
 
         if unsafe {
@@ -172,7 +180,7 @@ impl Capture {
                 &mut this.fmt_ctx,
                 PSTR::from(options.source.id.as_str()).as_ptr(),
                 format,
-                null_mut(),
+                &mut format_options,
             )
         } != 0
         {
@@ -191,9 +199,8 @@ impl Capture {
         // Desktop capture generally has only one stream.
         let streams = unsafe { std::slice::from_raw_parts(ctx_ref.streams, 1) };
         let stream = unsafe { &*(streams[0]) };
-        let codecpar = unsafe { &*stream.codecpar };
 
-        let codec = unsafe { avcodec_find_decoder(codecpar.codec_id) };
+        let codec = unsafe { avcodec_find_decoder({ &*stream.codecpar }.codec_id) };
         if codec.is_null() {
             return Err(ScreenCaptureError::NotFoundDecoder);
         }
@@ -207,75 +214,54 @@ impl Capture {
             return Err(ScreenCaptureError::SetParametersError);
         }
 
-        // let decoder_ctx = unsafe { &mut *this.codec_ctx };
-        // decoder_ctx.hw_device_ctx = unsafe { av_buffer_ref(this.hw_device_ctx) };
+        // unsafe {
+        //     let codec = &mut *this.codec_ctx;
+
+        //     let mut hw_device_ctx = null_mut();
+        //     if av_hwdevice_ctx_create(
+        //         &mut hw_device_ctx,
+        //         AVHWDeviceType::AV_HWDEVICE_TYPE_VAAPI,
+        //         null_mut(),
+        //         null_mut(),
+        //         0,
+        //     ) < 0
+        //     {
+        //         return Err(ScreenCaptureError::FailedToCreateHWDeviceContext);
+        //     }
+
+        //     let hw_frames_ctx = av_hwframe_ctx_alloc(hw_device_ctx);
+        //     if hw_frames_ctx.is_null() {
+        //         return Err(ScreenCaptureError::FailedToCreateHWFrameContext);
+        //     }
+
+        //     let frames_ctx = &mut *((&mut *hw_frames_ctx).data as *mut
+        // AVHWFramesContext);     frames_ctx.sw_format =
+        // AVPixelFormat::AV_PIX_FMT_BGR0;     frames_ctx.format =
+        // AVPixelFormat::AV_PIX_FMT_VAAPI;     frames_ctx.width = codec.width
+        // as i32;     frames_ctx.height = codec.height as i32;
+        //     frames_ctx.initial_pool_size = 5;
+
+        //     if av_hwframe_ctx_init(hw_frames_ctx) != 0 {
+        //         return Err(ScreenCaptureError::FailedToCreateHWFrameContext);
+        //     }
+
+        //     codec.hw_device_ctx = av_buffer_ref(hw_device_ctx);
+        //     codec.hw_frames_ctx = av_buffer_ref(hw_frames_ctx);
+        // }
 
         if unsafe { avcodec_open2(this.codec_ctx, codec, null_mut()) } != 0 {
             return Err(ScreenCaptureError::NotOpenDecoder);
         }
 
-        if unsafe {
-            avfilter_graph_create_filter(
-                &mut this.buffersrc_ctx,
-                avfilter_get_by_name(PSTR::from("buffer").as_ptr()),
-                PSTR::from("in").as_ptr(),
-                PSTR::from("video_size=2560x1440:pix_fmt=bgr0:time_base=1/60").as_ptr(),
-                null_mut(),
-                this.filter_graph,
-            )
-        } < 0
-        {
-            return Err(ScreenCaptureError::CreateSWScaleContextError);
-        }
-        
-        if unsafe {
-            avfilter_graph_create_filter(
-                &mut this.buffersink_ctx,
-                avfilter_get_by_name(PSTR::from("buffersink").as_ptr()),
-                PSTR::from("out").as_ptr(),
-                null_mut(),
-                null_mut(),
-                this.filter_graph,
-            )
-        } < 0
-        {
-            return Err(ScreenCaptureError::CreateSWScaleContextError);
-        }
-
-        let mut outputs = unsafe { avfilter_inout_alloc() };
-        unsafe {
-            let outputs = &mut *outputs;
-            outputs.name = av_strdup(PSTR::from("in").as_ptr());
-            outputs.filter_ctx = this.buffersrc_ctx;
-            outputs.next = null_mut();
-            outputs.pad_idx = 0;
-        }
-
-        let mut inputs = unsafe { avfilter_inout_alloc() };
-        unsafe {
-            let inputs = &mut *inputs;
-            inputs.name = av_strdup(PSTR::from("out").as_ptr());
-            inputs.filter_ctx = this.buffersink_ctx;
-            inputs.next = null_mut();
-            inputs.pad_idx = 0;
-        }
-
-        if unsafe {
-            avfilter_graph_parse_ptr(
-                this.filter_graph,
-                PSTR::from("hwmap=derive_device=vaapi,hwdownload,format=bgr0").as_ptr(),
-                &mut inputs,
-                &mut outputs,
-                null_mut()
-            )
-        } < 0
-        {
-            return Err(ScreenCaptureError::CreateSWScaleContextError);
-        }
-
-        if unsafe { avfilter_graph_config(this.filter_graph, null_mut()) } < 0 {
-            return Err(ScreenCaptureError::CreateSWScaleContextError);
-        }
+        // unsafe {
+        //     let filter_graph = &*this.filter_graph;
+        //     for filter in std::slice::from_raw_parts_mut(
+        //         filter_graph.filters,
+        //         filter_graph.nb_filters as usize,
+        //     ) {
+        //         { &mut **filter }.hw_device_ctx = { &*this.codec_ctx }.hw_device_ctx;
+        //     }
+        // }
 
         Ok(this)
     }
@@ -299,23 +285,119 @@ impl Capture {
             return None;
         }
 
-        println!("============== 444");
-        if unsafe { av_buffersrc_add_frame_flags(self.buffersrc_ctx, self.frames[0], 4) } < 0 {
+        if self.buffersink_ctx.is_null() {
+            self.init_filters()?;
+        }
+
+        if unsafe {
+            av_buffersrc_add_frame_flags(
+                self.buffersrc_ctx,
+                self.frames[0],
+                AV_BUFFERSRC_FLAG_KEEP_REF as i32,
+            )
+        } < 0
+        {
             return None;
         }
 
-        println!("============== 555");
         if unsafe { av_buffersink_get_frame(self.buffersink_ctx, self.frames[1]) } < 0 {
             return None;
         }
 
-        println!("============== 666");
-        if unsafe { av_hwframe_transfer_data(self.frames[2], self.frames[1], 0) } < 0 {
+        Some(unsafe { &*self.frames[1] })
+    }
+
+    fn init_filters(&mut self) -> Option<()> {
+        if unsafe {
+            avfilter_graph_create_filter(
+                &mut self.buffersrc_ctx,
+                avfilter_get_by_name(PSTR::from("buffer").as_ptr()),
+                PSTR::from("in").as_ptr(),
+                PSTR::from({
+                    let codec = &*self.codec_ctx;
+                    format!(
+                        "video_size={}x{}:pix_fmt={}:time_base=1/{}:pixel_aspect={}/{}",
+                        codec.width,
+                        codec.height,
+                        codec.pix_fmt as i32,
+                        self.options.fps,
+                        codec.sample_aspect_ratio.num,
+                        codec.sample_aspect_ratio.den
+                    )
+                })
+                .as_ptr(),
+                null_mut(),
+                self.filter_graph,
+            )
+        } < 0
+        {
             return None;
         }
 
-        println!("============== 777");
-        Some(unsafe { &*self.frames[2] })
+        if unsafe {
+            avfilter_graph_create_filter(
+                &mut self.buffersink_ctx,
+                avfilter_get_by_name(PSTR::from("buffersink").as_ptr()),
+                PSTR::from("out").as_ptr(),
+                null_mut(),
+                null_mut(),
+                self.filter_graph,
+            )
+        } < 0
+        {
+            return None;
+        }
+
+        unsafe {
+            let par = av_buffersrc_parameters_alloc();
+            { &mut *par }.hw_frames_ctx = { &*self.frames[0] }.hw_frames_ctx;
+
+            if av_buffersrc_parameters_set(self.buffersrc_ctx, par) < 0 {
+                return None;
+            }
+        }
+
+        let mut outputs = unsafe { avfilter_inout_alloc() };
+        let mut inputs = unsafe { avfilter_inout_alloc() };
+
+        unsafe {
+            let outputs = &mut *outputs;
+            outputs.name = av_strdup(PSTR::from("in").as_ptr());
+            outputs.filter_ctx = self.buffersrc_ctx;
+            outputs.pad_idx = 0;
+            outputs.next = null_mut();
+        }
+
+        unsafe {
+            let inputs = &mut *inputs;
+            inputs.name = av_strdup(PSTR::from("out").as_ptr());
+            inputs.filter_ctx = self.buffersink_ctx;
+            inputs.pad_idx = 0;
+            inputs.next = null_mut();
+        }
+
+        if unsafe {
+            avfilter_graph_parse_ptr(
+            self.filter_graph,
+            PSTR::from(format!(
+                "hwmap=derive_device=vaapi,scale_vaapi=w={}:h={}:format=nv12,hwdownload,format=nv12",
+                self.options.size.width, self.options.size.height
+            ))
+            .as_ptr(),
+            &mut inputs,
+            &mut outputs,
+            null_mut(),
+        )
+        } < 0
+        {
+            return None;
+        }
+
+        if unsafe { avfilter_graph_config(self.filter_graph, null_mut()) } < 0 {
+            return None;
+        }
+
+        Some(())
     }
 }
 
