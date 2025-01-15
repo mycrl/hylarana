7fc75d05ff84ca4f58a671c28d67323efa9674db
Lazy Panda <lepidodendraceae@gmail.com>
2025-01-15 21:47:04 +0800

    Update Codec.kt

--- sdk/android/hylarana/src/main/java/com/github/mycrl/hylarana/Codec.kt
+++ sdk/android/hylarana/src/main/java/com/github/mycrl/hylarana/Codec.kt
@@ -1,17 +1,20 @@
 package com.github.mycrl.hylarana
 
+import android.R.attr.mimeType
 import android.media.AudioFormat
 import android.media.AudioRecord
 import android.media.AudioTrack
 import android.media.MediaCodec
 import android.media.MediaCodecInfo
+import android.media.MediaCodecList
 import android.media.MediaFormat
 import android.os.Build
 import android.os.Process
 import android.util.Log
 import android.view.Surface
-import java.lang.Exception
 import java.nio.ByteBuffer
+import java.util.Locale
+
 
 abstract class ByteArraySinker {
     abstract fun sink(info: StreamBufferInfo, buf: ByteArray)
@@ -52,8 +55,6 @@ class Video {
                     MediaCodecInfo.CodecProfileLevel.AVCLevel5
                 }
             )
-
-            Log.i("================", "==================================== ${codec.name}")
 
             if (codec.name.indexOf(".rk.") >= 0) {
                 format.setInteger(MediaFormat.KEY_COMPLEXITY, 0)
@@ -165,6 +166,17 @@ class Video {
         private var worker: Thread
 
         init {
+            run {
+                val codecList = MediaCodecList(MediaCodecList.REGULAR_CODECS)
+                val codecInfos = codecList.codecInfos
+
+                for (codecInfo in codecInfos) {
+                    if (!codecInfo.isEncoder && codecInfo.isHardwareAccelerated) {
+                        Log.i("================", "============= ${codecInfo.name}");
+                    }
+                }
+            }
+
             val format = MediaFormat.createVideoFormat(MediaFormat.MIMETYPE_VIDEO_AVC, 2560, 1660)
             format.setInteger(MediaFormat.KEY_COLOR_FORMAT, MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface)
             format.setInteger(MediaFormat.KEY_BITRATE_MODE, MediaCodecInfo.EncoderCapabilities.BITRATE_MODE_VBR)
